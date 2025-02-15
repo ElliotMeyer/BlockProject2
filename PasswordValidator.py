b1d2b91c9f4598057455887772d0fe9d57ffb9b2
Elliot Meyer <elliotmyr@gmail.com>
2025-02-15 07:29:55 -0700

    Added tests for passwordValidator

--- PasswordValidator.py
+++ PasswordValidator.py
@@ -1,39 +1,56 @@
 #This file aims to validate the security of a user inputted password. 
 #Since I am using an open source license, I will let people contribute by adding to popular passwords that they can think of.
-popularPhrases = ["Password1!", "123456789L!", "qwerty123!", "abcde123!", "Password123!"]
 
 #//* 
 #User input needed, input your password and run the program to test the
-#password's security
+#password's security.
 #*//*
-password = ""
-
-hasDigit = False
-hasCapital = False
-hasSpecial = False
-hasPopularPhrase = False
-
-
-if popularPhrases.__contains__(password):
-   hasPopularPhrase = True
+def checkPassword(password):
+    
+    hasDigit = False
+    hasCapital = False
+    hasSpecial = False
+    hasPopularPhrase = False
+    popularPhrases = ["Password1!", "123456789L!", "qwerty123!", "abcde123!", "Password123!"]
 
 
-for character in password:
-    if not character.isalpha():
-        print("Password is not secure")
-        password = ""
-        break
-    elif character.isdigit():
-        hasDigit = True
-        break
-    elif character.isupper():
-        hasCapital = True
-        break
+    if password in popularPhrases:
+        hasPopularPhrase = True
+
+    
+    for character in password:
+        if character.isdigit():
+            hasDigit = True
+        elif character.isupper():
+            hasCapital = True
+        elif not character.isalnum():
+            hasSpecial = True
+
+
+
+    if len(password) < 8 or not hasDigit or not hasCapital or not hasSpecial or hasPopularPhrase: 
+        print("Password is not secure. Try again!")
     else:
         print("Password is secure")
-        break
 
+# Simple tests to confirm the program works correctly.
 
-if len(password) < 8 or not hasDigit or not hasCapital or not hasSpecial or not hasPopularPhrase: 
-    print("Password is not secure. Try again!")
-    password = ""
+# Test 1 #Expect not secure, common password.
+password1 = "Password1!"
+checkPassword(password1)
+
+# Test 2 #Expect secure
+password2 = "SecurePass123!"
+checkPassword(password2)
+
+# Test 3 #Expect not secure, too short
+password3 = "Pass1!"
+checkPassword(password3)
+
+# Test 4 #Expect not secure, missing digit
+password4 = "Password!"
+checkPassword(password4)
+
+# Test 5 #Expect not secure, missing special character
+password5 = "Password1232313"
+checkPassword(password5)
